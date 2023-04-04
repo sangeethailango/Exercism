fd8e4405a245473ef4770bf8f0c950d12eb7cd77
sangeetha <sangeethailango21@gmail.com>
2023-04-04 16:42:57 +0530

    log level

---
+++ log-level/lib/log_level.ex
@@ -0,0 +1,22 @@
+defmodule LogLevel do
+  def to_label(level, legacy?) do
+    cond do
+      legacy? == false && level == 0 -> :trace
+      (legacy? == false || true) && level == 1 -> :debug
+      (legacy? == false || true) && level == 2 -> :info
+      (legacy? == false || true) && level == 3 -> :warning
+      (legacy? == false || true) && level == 4 -> :error
+      legacy? == false && level == 5 -> :fatal
+      true -> :unknown
+    end
+  end
+
+  def alert_recipient(level, legacy?) do
+    cond do
+      to_label(level, legacy?) == :error || to_label(level, legacy?) == :fatal -> :ops
+      to_label(level, legacy?) == :unknown -> :dev1
+      !to_label(level, legacy?) -> :dev2
+      true -> false
+    end
+  end
+end
