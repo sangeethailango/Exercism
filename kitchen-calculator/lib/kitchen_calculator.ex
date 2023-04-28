974028ec47cddc1534a3cc289abd9ae3f03434d4
sangeetha <sangeethailango21@gmail.com>
2023-04-28 15:51:41 +0530

    Modification

--- kitchen-calculator/lib/kitchen_calculator.ex
+++ kitchen-calculator/lib/kitchen_calculator.ex
@@ -3,56 +3,48 @@ defmodule KitchenCalculator do
     elem(volume_pair, 1)
   end
 
-  def to_milliliter(volume_pair = {:milliliter, _}) do
-    {:milliliter, elem(volume_pair, 1)}
+  def to_milliliter({:milliliter, quantity}) do
+    {:milliliter, quantity}
   end
 
-  def to_milliliter(volume_pair = {:cup, _}) do
-    cup_to_ml = elem(volume_pair, 1) * 240
-    {:milliliter, cup_to_ml}
+  def to_milliliter({:cup, quantity}) do
+    {:milliliter, quantity * 240}
   end
 
-  def to_milliliter(volume_pair = {:fluid_ounce, _}) do
-    fluid_ounce_to_ml = elem(volume_pair, 1) * 30
-    {:milliliter, fluid_ounce_to_ml}
+  def to_milliliter({:fluid_ounce, quantity}) do
+    {:milliliter, quantity * 30}
   end
 
-  def to_milliliter(volume_pair = {:teaspoon, _}) do
-    teaspoon_to_ml = elem(volume_pair, 1) * 5
-    {:milliliter, teaspoon_to_ml}
+  def to_milliliter({:teaspoon, quantity}) do
+    {:milliliter, quantity * 5}
   end
 
-  def to_milliliter(volume_pair = {:tablespoon, _}) do
-    tablespoon_to_ml = elem(volume_pair, 1) * 15
-    {:milliliter, tablespoon_to_ml}
+ def to_milliliter({:tablespoon, quantity}) do
+    {:milliliter, quantity * 15}
   end
 
-  def from_milliliter(volume_pair, unit = :milliliter) do
-    {unit, elem(volume_pair, 1)}
+  def from_milliliter({:milliliter, quantity},  :milliliter) do 
+   {:milliliter, quantity}
+  end
+  
+  def from_milliliter({:milliliter, quantity},  :cup) do
+    {:cup, quantity / 240}
   end
 
-  def from_milliliter(volume_pair, unit = :cup) do
-    ml_to_cup = elem(volume_pair, 1) / 240
-    {unit, ml_to_cup}
+  def from_milliliter({:milliliter, quantity},  :fluid_ounce) do
+   {:fluid_ounce, quantity / 30}
+  end
+  
+  def from_milliliter({:milliliter, quantity},  :teaspoon) do
+   {:teaspoon, quantity / 5}
   end
 
-  def from_milliliter(volume_pair, unit = :fluid_ounce) do
-    ml_to_fluid_ounce = elem(volume_pair, 1) / 30
-    {unit, ml_to_fluid_ounce}
-  end
-
-  def from_milliliter(volume_pair, unit = :teaspoon) do
-    ml_to_teaspoon = elem(volume_pair, 1) / 5
-    {unit, ml_to_teaspoon}
-  end
-
-  def from_milliliter(volume_pair, unit = :tablespoon) do
-    ml_to_tablespoon = elem(volume_pair, 1) / 15
-    {unit, ml_to_tablespoon}
+  def from_milliliter({:milliliter, quantity}, :tablespoon) do
+   {:tablespoon, quantity / 15}
   end
 
   def convert(volume_pair, unit) do
-    to_milliliter = to_milliliter(volume_pair)
-    from_milliliter(to_milliliter, unit)
+   to_milliliter = to_milliliter(volume_pair)
+   from_milliliter(to_milliliter, unit)
   end
 end
