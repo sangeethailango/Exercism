e42eb80a54331ee784bb0c53078c7033eeaac608
sangeetha <sangeethailango21@gmail.com>
2023-05-02 17:27:38 +0530

    Change boolean operands

--- pacman-rules/lib/rules.ex
+++ pacman-rules/lib/rules.ex
@@ -8,10 +8,10 @@ defmodule Rules do
   end
 
   def lose?(power_pellet_active, touching_ghost) do
-     power_pellet_active== false && touching_ghost 
+     not power_pellet_active && touching_ghost 
   end
 
   def win?(has_eaten_all_dots, power_pellet_active, touching_ghost) do
-     has_eaten_all_dots && touching_ghost == false || power_pellet_active 
+     has_eaten_all_dots && not touching_ghost  || power_pellet_active 
   end
 end
