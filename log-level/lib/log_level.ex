a8a6e26a8d0c5f265a629eb8e2d767cab662d47f
sangeetha <sangeethailango21@gmail.com>
2023-05-02 17:08:49 +0530

    Indentation

--- log-level/lib/log_level.ex
+++ log-level/lib/log_level.ex
@@ -6,17 +6,17 @@ defmodule LogLevel do
       level == 2 -> :info
       level == 3 -> :warning
       level == 4 -> :error
-      not legacy?  && level == 5 -> :fatal
-     true -> :unknown 
+      not legacy? && level == 5 -> :fatal
+      true -> :unknown 
     end
   end
 
   def alert_recipient(level, legacy?) do
     cond do
-     to_label(level, legacy?) == :error||to_label(level, legacy?) == :fatal -> :ops
-     to_label(level, legacy?) == :unknown && legacy? -> :dev1
-     to_label(level, legacy?) == :unknown && not legacy? -> :dev2
-     true -> false
+      to_label(level, legacy?) == :error || to_label(level, legacy?) == :fatal -> :ops
+      to_label(level, legacy?) == :unknown && legacy? -> :dev1
+      to_label(level, legacy?) == :unknown && not legacy? -> :dev2
+      true -> false
     end  
   end
 end  
