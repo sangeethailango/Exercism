a9fd92599d60c8deb70142b12822c0fa2aab4304
sangeetha <sangeethailango21@gmail.com>
2023-05-02 16:01:16 +0530

    Modification

--- log-level/lib/log_level.ex
+++ log-level/lib/log_level.ex
@@ -1,22 +1,22 @@
 defmodule LogLevel do
   def to_label(level, legacy?) do
     cond do
-      legacy? == false && level == 0 -> :trace
-      (legacy? == false || true) && level == 1 -> :debug
-      (legacy? == false || true) && level == 2 -> :info
-      (legacy? == false || true) && level == 3 -> :warning
-      (legacy? == false || true) && level == 4 -> :error
-      legacy? == false && level == 5 -> :fatal
-      true -> :unknown
+      not legacy? && level == 0 -> :trace
+      level == 1 -> :debug
+      level == 2 -> :info
+      level == 3 -> :warning
+      level == 4 -> :error
+      not legacy?  && level == 5 -> :fatal
+     true -> :unknown 
     end
   end
 
   def alert_recipient(level, legacy?) do
     cond do
-      to_label(level, legacy?) == :error || to_label(level, legacy?) == :fatal -> :ops
-      to_label(level, legacy?) == :unknown && legacy? == true -> :dev1
-      to_label(level, legacy?) == :unknown && legacy? == false -> :dev2
-      true -> false
-    end
+     to_label(level, legacy?) == :error||to_label(level, legacy?) == :fatal -> :ops
+     to_label(level, legacy?) == :unknown && legacy? -> :dev1
+     to_label(level, legacy?) == :unknown && not legacy? -> :dev2
+     true -> false
+    end  
   end
-end
+end  
